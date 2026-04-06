4e5b511b64bc72839a18dc40b416835334c12a53
Ram <rammohanofficial03@gmail.com>
2026-04-07 02:00:29 +0530

    Minor change

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -15,7 +15,7 @@ export const env = createEnv({
 
             return{
                 ...rest,
-                DATABASE_URL: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`
+                DATABASE_URL: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&channel_binding=require`
             }
         })
     },
